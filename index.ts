096e81b2b902c16c99a1853dc5da883ddc3a8a8d
taf-s <tafara.sithole@gmail.com>
2024-08-09 03:34:18 +0200

    debbugging

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { showReviewTotal, populateUser, showDetails, getTopTwoReviews} from './utils'
 import { Permissions , LoyaltyUser } from './enums'
 import { Review, Property } from './interfaces'
-import MainProperty from './classes.ts' 
+import MainProperty from './classes' 
 const propertyContainer = document.querySelector('.properties')
 const reviewContainer = document.querySelector('.reviews')
 const container = document.querySelector('.container')
